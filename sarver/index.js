79cb2195088e812c382efe195f1ea77233c188a6
yosida_ayane <yosida.ayane@gmail.com>
2020-06-29 17:35:21 +0900

    丸がボタンを押すたび大きくなる。

--- sarver/index.js
+++ sarver/index.js
@@ -7,30 +7,50 @@ const app = express();
 const httpServer = http.Server(app);
 // httpServer を元に WebSocket（リアルタイム通信）用のIOを作成
 const io = new SocketServer(httpServer);
+
+let diameter = 0;
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   // ひとつ上のディレクトリにある index.html を返します
   const file = path.join(__dirname, '../index.html');
   res.sendFile(file);
 });
+
+app.get('/contents', (req, res) => {
+  // ひとつ上のディレクトリにある contents.html を返します
+  const file = path.join(__dirname, '../contents.html');
+  res.sendFile(file);
+});
+
 app.get('/test', (req, res) => {
   res.send('test');
 });
+
 io.on('connection', (socket) => {
 	// ユーザーが新規に接続された時にこの中が実行されます
 	console.log('ユーザーが接続しました');
-	// ユーザーからメッセージを受信した時の処理を.on で登録します
+  // ユーザーからメッセージを受信した時の処理を.on で登録します
 	socket.on('button0Click', (msg) => {
 		// ユーザーからメッセージを受信した時の処理
 		console.log('ユーザーからのメッセージを受信しました',msg);
 		// このサーバーに接続しているユーザーに受信したメッセージを配信します
 		io.emit('button0Click', msg * 100);
-    });
+  });
     
-    socket.on('button1Click',(msg) =>{
-        console.log('ユーザーからのメッセージを受信しました',msg);
-        io.emit('button1Click',msg * 1000);
-    });
+  socket.on('button1Click',(msg) =>{
+    console.log('ユーザーからのメッセージを受信しました',msg);
+    io.emit('button1Click',msg * 1000);
+  });
+
+  socket.on('diameter',(msg) =>{
+    console.log('ユーザーからの直径を受信しました',msg);
+    diameter = diameter + 10;
+    io.emit('diameter',diameter);
+  });
 });
+
 httpServer.listen(3000, function(){
   console.log('サーバーが起動しました。URLは http://localhost:3000 です');
 });
