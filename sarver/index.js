d8a1c48833eb885b3ada573f7b686c967f9e7887
yosida_ayane <yosida.ayane@gmail.com>
2020-07-06 17:20:05 +0900

    Three colors (プロトタイプ展用デモ)

--- sarver/index.js
+++ sarver/index.js
@@ -9,6 +9,8 @@ const httpServer = http.Server(app);
 const io = new SocketServer(httpServer);
 
 let diameter = 0;
+let diameter1 = 0;
+let diameter2 = 0;
 
 app.use(express.static('public'));
 
@@ -30,27 +32,64 @@ app.get('/test', (req, res) => {
 
 io.on('connection', (socket) => {
 	// ユーザーが新規に接続された時にこの中が実行されます
-	console.log('ユーザーが接続しました');
+  console.log('ユーザーが接続しました');
+  
+//RED
   // ユーザーからメッセージを受信した時の処理を.on で登録します
 	socket.on('button0Click', (msg) => {
 		// ユーザーからメッセージを受信した時の処理
-		console.log('ユーザーからのメッセージを受信しました',msg);
+		console.log('ユーザーからの赤メッセージを受信しました',msg);
 		// このサーバーに接続しているユーザーに受信したメッセージを配信します
-		io.emit('button0Click', msg * 100);
-  });
-    
-  socket.on('button1Click',(msg) =>{
-    console.log('ユーザーからのメッセージを受信しました',msg);
-    io.emit('button1Click',msg * 1000);
+		io.emit('button0Click', msg = 'R');
   });
 
   socket.on('diameter',(msg) =>{
-    console.log('ユーザーからの直径を受信しました',msg);
-    diameter = diameter + 10;
+    console.log('ユーザーからの赤の高さを受信しました',msg);
+    diameter = diameter + 100;
     io.emit('diameter',diameter);
   });
+
+    
+//BLUE
+  socket.on('button1Click',(msg) =>{
+    console.log('ユーザーからの青メッセージを受信しました',msg);
+    io.emit('button1Click',msg = 'B');
+  });
+
+  socket.on('diameter1',(msg) =>{
+    console.log('ユーザーからの青の高さを受信しました',msg);
+    diameter1 = diameter1 + 100;
+    io.emit('diameter1',diameter1);
+  });
+
+  //YELLOW
+  socket.on('button2Click',(msg) =>{
+    console.log('ユーザーからの黄メッセージを受信しました',msg);
+    io.emit('button2Click',msg = 'Y');
+  });
+
+  socket.on('diameter2',(msg) =>{
+    console.log('ユーザーからの黄の高さを受信しました',msg);
+    diameter2 = diameter2 + 100;
+    io.emit('diameter2',diameter2);
+  });
+
+  var reset = function(){
+    console.log('リセットします');
+
+    diameter = diameter = 0;
+    io.emit('diameter',diameter);
+
+    diameter1 = diameter1 = 0;
+    io.emit('diameter1',diameter1);
+
+    diameter2 = diameter2 = 0;
+    io.emit('diameter2',diameter2);
+  };
+  
+  setInterval( reset, 1000 * 10 );
 });
 
-httpServer.listen(3000, function(){
-  console.log('サーバーが起動しました。URLは http://localhost:3000 です');
+httpServer.listen(3003, function(){
+  console.log('サーバーが起動しました。URLは http://localhost:3003 です');
 });
