41058e6fa99be05d357764124f3441e38bf7731d
yosida_ayane <yosida.ayane@gmail.com>
2020-10-12 11:38:21 +0900

    コードを整えた

--- sarver/public/shared/js/contents.js
+++ sarver/public/shared/js/contents.js
@@ -1,32 +1,31 @@
 const pathList = document.querySelectorAll('.path');
 
-console.log(gsap);
-
 console.log(pathList);
 
 //pathList.item(Math.round(Math.random() * pathList.length)).classList.add('path-show');
 
-  const socket = io();
+const socket = io();
+const BUTTON_NUM = 29;//スマホ側のボタンの数に合わせる
 
-  //アニメーションが始まる前の見た目を設定する
-  gsap.set(pathList,{
-    scale: 0,//縮尺を０倍にする
-    opacity: 0,//透明にする
-    transformOrigin: "50% 50%",//変形の基点を図形の重心にする
-  });
+//アニメーションが始まる前の見た目を設定する
+gsap.set(pathList,{
+  scale: 0,//縮尺を０倍にする
+  opacity: 0,//透明にする
+  transformOrigin: "50% 50%",//変形の基点を図形の重心にする
+});
 
-  socket.on('button0Click',function(msg){
+for (let i = 0; i < BUTTON_NUM; i++){
+  socket.on(`button${i}Click`,(msg) => {
     gsap.to(
       //動かしたい要素や素材
-      pathList.item(0),
-      {
+      pathList.item(i),{
         duration: 0.4, //アニメーションする時間
         scale: 1,
         opacity: 1,
-        onComplete: function(){
+        onComplete: () => {
           //消える時のアニメーションを書く
           gsap.to(
-            pathList.item(0),{
+            pathList.item(i),{
               delay: 0.2,//　何秒後にこのアニメーションを始めるか
               duration: 0.4,
               opacity: 0,
@@ -35,118 +34,5 @@ console.log(pathList);
         }
       }
     );
-
-    // pathList.item(0).classList.add('path-show');
   });
-
-  socket.on('button1Click',function(msg){
-    pathList.item(1).classList.add('path-show');
-  });
-
-  socket.on('button2Click',function(msg){
-    pathList.item(2).classList.add('path-show');
-  });
-
-  socket.on('button3Click',function(msg){
-    pathList.item(3).classList.add('path-show');
-  });
-
-  socket.on('button4Click',function(msg){
-    pathList.item(4).classList.add('path-show');
-  });
-
-  socket.on('button5Click',function(msg){
-    pathList.item(5).classList.add('path-show');
-  });
-
-  socket.on('button6Click',function(msg){
-    pathList.item(6).classList.add('path-show');
-  });
-
-  socket.on('button7Click',function(msg){
-    pathList.item(7).classList.add('path-show');
-  });
-
-  socket.on('button8Click',function(msg){
-    pathList.item(8).classList.add('path-show');
-  });
-
-  socket.on('button9Click',function(msg){
-    pathList.item(9).classList.add('path-show');
-  });
-
-  socket.on('button10Click',function(msg){
-    pathList.item(10).classList.add('path-show');
-  });
-
-  socket.on('button11Click',function(msg){
-    pathList.item(11).classList.add('path-show');
-  });
-
-  socket.on('button12Click',function(msg){
-    pathList.item(12).classList.add('path-show');
-  });
-
-  socket.on('button13Click',function(msg){
-    pathList.item(13).classList.add('path-show');
-  });
-
-  socket.on('button14Click',function(msg){
-    pathList.item(14).classList.add('path-show');
-  });
-
-  socket.on('button15Click',function(msg){
-    pathList.item(15).classList.add('path-show');
-  });
-
-  socket.on('button16Click',function(msg){
-    pathList.item(16).classList.add('path-show');
-  });
-
-  socket.on('button17Click',function(msg){
-    pathList.item(17).classList.add('path-show');
-  });
-
-  socket.on('button18Click',function(msg){
-    pathList.item(18).classList.add('path-show');
-  });
-
-  socket.on('button19Click',function(msg){
-    pathList.item(19).classList.add('path-show');
-  });
-
-  socket.on('button20Click',function(msg){
-    pathList.item(20).classList.add('path-show');
-  });
-
-  socket.on('button21Click',function(msg){
-    pathList.item(21).classList.add('path-show');
-  });
-
-  socket.on('button22Click',function(msg){
-    pathList.item(22).classList.add('path-show');
-  });
-
-  socket.on('button23Click',function(msg){
-    pathList.item(23).classList.add('path-show');
-  });
-
-  socket.on('button24Click',function(msg){
-    pathList.item(24).classList.add('path-show');
-  });
-
-  socket.on('button25Click',function(msg){
-    pathList.item(25).classList.add('path-show');
-  });
-
-  socket.on('button26Click',function(msg){
-    pathList.item(26).classList.add('path-show');
-  });
-
-  socket.on('button27Click',function(msg){
-    pathList.item(27).classList.add('path-show');
-  });
-
-  socket.on('button28Click',function(msg){
-    pathList.item(28).classList.add('path-show');
-  });
+}
